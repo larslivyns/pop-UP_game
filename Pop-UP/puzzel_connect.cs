136af46f7c72343ae09b9232781c7d65b427638f
Lars  Livyns <lars.livyns@leerling.go-ao.be>
2026-03-26 10:06:42 +0100

    beetje_extra_erbij

--- Pop-UP/puzzel_connect.cs
+++ Pop-UP/puzzel_connect.cs
@@ -2,22 +2,35 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Pop_UP
 {
-    internal class puzzel_connect : Form1
+    internal class puzzel_connect : Form
     {
         public string naam;
-
-        public event EventHandler Ready;
+        
         public puzzel_connect(string naam)
         {
-            Label lbl_test = new Label();
-            lbl_test.Text = naam;
-            Controls.Add(lbl_test);
+            this.Size = new Size(266,290);
+            
+            //rnd een aantal vakjes vulln me kleurkes
+            //speelveld opzettn
+            for (int i = 0; i < 25; i++)
+            {
+                PictureBox grid_pb = new PictureBox();
+                grid_pb.Size = new Size(50,50);
+                grid_pb.Location = new Point((i % 5) * 50, (i / 5) * 50);
+                grid_pb.BorderStyle = BorderStyle.FixedSingle;
+                Controls.Add(grid_pb);
+
+                Random rnd = new Random();
+                int gekleurd_vierkant = rnd.Next(0, 3);
+                Console.WriteLine(gekleurd_vierkant.ToString());
+            }
         }
     }
 }
